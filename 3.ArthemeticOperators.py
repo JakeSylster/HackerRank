86b01f7d7c674b724299d9a6de7a891b5df7a6d9
Ajay Golla <44961432+JakeSylster@users.noreply.github.com>
2026-04-27 12:49:16 -0400

    Add task comments for arithmetic operations
    Add task description for arithmetic operations.

--- 3.ArthemeticOperators.py
+++ 3.ArthemeticOperators.py
@@ -1,3 +1,4 @@
+#Task
 # The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
 # The first line contains the sum of the two numbers.
 # The second line contains the difference of the two numbers (first - second).
